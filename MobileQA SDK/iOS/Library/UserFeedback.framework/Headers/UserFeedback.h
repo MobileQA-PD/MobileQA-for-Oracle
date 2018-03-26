08b3a6730031956123d1a8692dbabb9fd09e38c6
Lee Seung Woo <swlee@peopledream.kr>
2018-03-26 15:03:07 +0900

    디렉터리 수정 등등

---
+++ MobileQA SDK/iOS/Library/UserFeedback.framework/Headers/UserFeedback.h
@@ -0,0 +1,24 @@
+//
+//  UserFeedback.h
+//  UserFeedback
+//
+//  Created by 이승우 on 2018. 3. 19..
+//  Copyright © 2018년 이승우. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UserFeedback.
+FOUNDATION_EXPORT double UserFeedbackVersionNumber;
+
+//! Project version string for UserFeedback.
+FOUNDATION_EXPORT const unsigned char UserFeedbackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserFeedback/PublicHeader.h>
+
+
+@interface UserFeedback : NSObject
++ (void) setFeedback:(NSString *)userId;
++ (void) showFeedback;
++ (NSString *) getUserId;
+@end
