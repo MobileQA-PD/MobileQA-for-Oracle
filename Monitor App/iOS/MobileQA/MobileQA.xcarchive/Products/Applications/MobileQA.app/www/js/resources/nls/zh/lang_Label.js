0b83ca8a4670f5bf1931297c0810ea6ce9308edf
Lee Seung Woo <swlee@peopledream.kr>
2018-04-02 17:55:22 +0900

    최신 apk 등록

---
+++ Monitor App/iOS/MobileQA/MobileQA.xcarchive/Products/Applications/MobileQA.app/www/js/resources/nls/zh/lang_Label.js
@@ -0,0 +1,17 @@
+define({
+    "MCSInfo": "MCS",
+    "Storage": "存储",
+    "CustomAPI": "自定义API",
+    "Analytics": "分析",
+    "dialog": {
+        "okay": "好的",
+        "cancel": "取消"
+    },
+    "button": {
+        "ok": "OK",
+        "cancel": "Cancel",
+        "login": "Login"
+    },
+    "companyName": "Oracle",
+    "appName": "Mobile QoS"    
+});
